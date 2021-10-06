8177cc2c5b72d92d9311ff03fe9dada85b02567a
Roberto Ramírez <robertrafyt2007@gmail.com>
2021-10-06 15:23:51 -0600

    feat: move live cursors chat reactions to index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,32 +1,298 @@
-import React from "react";
+/**
+ * This file shows how to create Live Cursors with a small chat and interactions
+ *
+ * Because it's a bit more advanced that others examples, it's implemented using typescript to ensure that we introduce less bug while maintaining it.
+ * It also uses Tailwind CSS for the styling
+ */
+
+ import {
+  RoomProvider,
+  useBroadcastEvent,
+  useEventListener,
+  useMyPresence,
+  useOthers,
+} from "@liveblocks/react";
+import { useState, useCallback, useEffect } from "react";
 import Head from "next/head";
-import ListItem from "../components/ListItem";
-import SingleLineCodeBlock from "../components/SingleLineCodeBlock";
-import InlineCodeBlock from "../components/InlineCodeBlock";
+import Cursor from "../components/CursorWithMessage";
+import FlyingReaction from "../components/FlyingReaction";
+import ReactionSelector from "../components/ReactionSelector";
+import useInterval from "../components/useInterval";
 
-export async function getStaticProps() {
-  return {
-    props: {
-      isRunningOnCodeSandbox: process.env.CODESANDBOX_SSE != null,
-      hasSetupLiveblocksKey:
-        process.env.NEXT_PUBLIC_LIVEBLOCKS_PUBLIC_KEY != null,
-    },
-  };
-}
+const COLORS = ["#DC2626", "#D97706", "#059669", "#7C3AED", "#DB2777"];
 
-type Props = {
-  hasSetupLiveblocksKey: boolean;
-  isRunningOnCodeSandbox: boolean;
+type Presence = {
+  cursor: {
+    x: number;
+    y: number;
+  } | null;
+  message: string;
 };
 
-export default function Home({
-  hasSetupLiveblocksKey,
-  isRunningOnCodeSandbox,
-}: Props) {
+enum CursorMode {
+  Hidden,
+  Chat,
+  ReactionSelector,
+  Reaction,
+}
+
+type CursorState =
+  | {
+      mode: CursorMode.Hidden;
+    }
+  | {
+      mode: CursorMode.Chat;
+      message: string;
+      previousMessage: string | null;
+    }
+  | {
+      mode: CursorMode.ReactionSelector;
+    }
+  | {
+      mode: CursorMode.Reaction;
+      reaction: string;
+      isPressed: boolean;
+    };
+
+type Reaction = {
+  value: string;
+  timestamp: number;
+  point: { x: number; y: number };
+};
+
+type ReactionEvent = {
+  x: number;
+  y: number;
+  value: string;
+};
+
+function LiveCursorsChatReactions() {
+  const others = useOthers<Presence>();
+  const [{ cursor }, updateMyPresence] = useMyPresence<Presence>();
+  const broadcast = useBroadcastEvent();
+  const [state, setState] = useState<CursorState>({ mode: CursorMode.Hidden });
+  const [reactions, setReactions] = useState<Reaction[]>([]);
+
+  const setReaction = useCallback((reaction: string) => {
+    setState({ mode: CursorMode.Reaction, reaction, isPressed: false });
+  }, []);
+
+  // Remove reactions that are not visible anymore (every 1 sec)
+  useInterval(() => {
+    setReactions((reactions) =>
+      reactions.filter((reaction) => reaction.timestamp > Date.now() - 4000)
+    );
+  }, 1000);
+
+  useInterval(() => {
+    if (state.mode === CursorMode.Reaction && state.isPressed && cursor) {
+      setReactions((reactions) =>
+        reactions.concat([
+          {
+            point: { x: cursor.x, y: cursor.y },
+            value: state.reaction,
+            timestamp: Date.now(),
+          },
+        ])
+      );
+      broadcast({
+        x: cursor.x,
+        y: cursor.y,
+        value: state.reaction,
+      });
+    }
+  }, 100);
+
+  useEffect(() => {
+    function onKeyUp(e: KeyboardEvent) {
+      if (e.key === "/") {
+        setState({ mode: CursorMode.Chat, previousMessage: null, message: "" });
+      } else if (e.key === "Escape") {
+        updateMyPresence({ message: "" });
+        setState({ mode: CursorMode.Hidden });
+      } else if (e.key === "e") {
+        setState({ mode: CursorMode.ReactionSelector });
+      }
+    }
+
+    window.addEventListener("keyup", onKeyUp);
+
+    function onKeyDown(e: KeyboardEvent) {
+      if (e.key === "/") {
+        e.preventDefault();
+      }
+    }
+
+    window.addEventListener("keydown", onKeyDown);
+
+    return () => {
+      window.removeEventListener("keyup", onKeyUp);
+      window.removeEventListener("keydown", onKeyDown);
+    };
+  }, [updateMyPresence]);
+
+  useEventListener(({ event }: { event: ReactionEvent }) => {
+    setReactions((reactions) =>
+      reactions.concat([
+        {
+          point: { x: event.x, y: event.y },
+          value: event.value,
+          timestamp: Date.now(),
+        },
+      ])
+    );
+  });
+
   return (
-    <div>
+    <>
+      <div
+        className="relative h-screen w-full flex items-center justify-center overflow-hidden"
+        style={{
+          cursor:
+            state.mode === CursorMode.Chat
+              ? "none"
+              : "url(cursor.svg) 0 0, auto",
+        }}
+        onPointerMove={(event) => {
+          if (cursor == null || state.mode !== CursorMode.ReactionSelector) {
+            updateMyPresence({
+              cursor: {
+                x: Math.round(event.clientX),
+                y: Math.round(event.clientY),
+              },
+            });
+          }
+        }}
+        onPointerLeave={() => {
+          setState({
+            mode: CursorMode.Hidden,
+          });
+          updateMyPresence({
+            cursor: null,
+          });
+        }}
+        onPointerDown={(event) => {
+          updateMyPresence({
+            cursor: {
+              x: Math.round(event.clientX),
+              y: Math.round(event.clientY),
+            },
+          });
+          setState((state) =>
+            state.mode === CursorMode.Reaction
+              ? { ...state, isPressed: true }
+              : state
+          );
+        }}
+        onPointerUp={() => {
+          setState((state) =>
+            state.mode === CursorMode.Reaction
+              ? { ...state, isPressed: false }
+              : state
+          );
+        }}
+      >
+        {reactions.map((reaction) => {
+          return (
+            <FlyingReaction
+              key={reaction.timestamp.toString()}
+              x={reaction.point.x}
+              y={reaction.point.y}
+              timestamp={reaction.timestamp}
+              value={reaction.value}
+            />
+          );
+        })}
+        {cursor && (
+          <div
+            className="absolute top-0 left-0"
+            style={{
+              transform: `translateX(${cursor.x}px) translateY(${cursor.y}px)`,
+            }}
+          >
+            {state.mode === CursorMode.Chat && (
+              <>
+                <img src="cursor.svg" />
+
+                <div
+                  className="absolute top-5 left-2 px-4 py-2 bg-blue-500 text-white leading-relaxed text-sm"
+                  onKeyUp={(e) => e.stopPropagation()}
+                  style={{
+                    borderRadius: 20,
+                  }}
+                >
+                  {state.previousMessage && <div>{state.previousMessage}</div>}
+                  <input
+                    className="bg-transparent border-none	outline-none text-white placeholder-blue-300 w-60"
+                    autoFocus={true}
+                    onChange={(e) => {
+                      updateMyPresence({ message: e.target.value });
+                      setState({
+                        mode: CursorMode.Chat,
+                        previousMessage: null,
+                        message: e.target.value,
+                      });
+                    }}
+                    onKeyDown={(e) => {
+                      if (e.key === "Enter") {
+                        setState({
+                          mode: CursorMode.Chat,
+                          previousMessage: state.message,
+                          message: "",
+                        });
+                      } else if (e.key === "Escape") {
+                        setState({
+                          mode: CursorMode.Hidden,
+                        });
+                      }
+                    }}
+                    placeholder={state.previousMessage ? "" : "Say something…"}
+                    value={state.message}
+                    maxLength={50}
+                  />
+                </div>
+              </>
+            )}
+            {state.mode === CursorMode.ReactionSelector && (
+              <ReactionSelector
+                setReaction={(reaction) => {
+                  setReaction(reaction);
+                }}
+              />
+            )}
+            {state.mode === CursorMode.Reaction && (
+              <div className="absolute top-3.5 left-1 pointer-events-none select-none">
+                {state.reaction}
+              </div>
+            )}
+          </div>
+        )}
+
+        {others.map(({ connectionId, presence }) => {
+          if (presence == null || !presence.cursor) {
+            return null;
+          }
+
+          return (
+            <Cursor
+              key={connectionId}
+              color={COLORS[connectionId % COLORS.length]}
+              x={presence.cursor.x}
+              y={presence.cursor.y}
+              message={presence.message}
+            />
+          );
+        })}
+      </div>
+    </>
+  );
+}
+
+export default function Room() {
+  return (
+    <>
       <Head>
-        <title>Liveblocks</title>
+        <title>Liveblocks - Live Cursors</title>
         <link
           rel="icon"
           type="image/png"
@@ -40,143 +306,42 @@ export default function Home({
           href="/favicon-16x16.png"
         />
       </Head>
-      <main className="container mx-auto px-8">
-        <h1 className="text-3xl font-semibold mt-24 mb-2">
-          Welcome to Liveblocks Next.js Live cursors examples
-        </h1>
-        {hasSetupLiveblocksKey ? (
-          <>
-            <p className="text-gray-400 mb-4 text-lg">
-              Get started with the real-time examples below.
-            </p>
+    <RoomProvider
+      id="example-live-cursors-chat-reactions"
+      defaultPresence={() => ({
+        cursor: null,
+        message: "",
+      })}
+    >
+      <div className="fixed inset-0 flex justify-center items-center select-none bg-white">
+        <div className="text-center max-w-sm">
+          <ul className="flex items-center justify-center space-x-2 mt-4">
+            <li className="flex items-center space-x-2 text-sm bg-gray-100 rounded-md py-2 px-3">
+              <span>Reactions</span>
+              <span className="block uppercase font-medium text-xs text-gray-500 rounded border border-gray-300 px-1">
+                E
+              </span>
+            </li>
 
-            <div className="grid grid-cols-3 gap-16">
-              <div>
-                <div className="max-w-sm">
-                  <h2 className="mt-12 mb-1 font-medium text-lg">Basic</h2>
-                  <p className="text-gray-400 mb-4">
-                    Basic examples to help you understand how the APIs work for
-                    each block.
-                  </p>
-                </div>
-                <ul className="grid grid-cols-1 gap-4">
-                  <ListItem
-                    label="Presence demo"
-                    href="/live-cursors-basic"
-                    description="Presence"
-                  />
-                </ul>
-              </div>
+            <li className="flex items-center space-x-2 text-sm bg-gray-100 rounded-md py-2 px-3">
+              <span>Chat</span>
+              <span className="block uppercase font-medium text-xs text-gray-500 rounded border border-gray-300 px-1">
+                /
+              </span>
+            </li>
 
-              <div className="col-span-2">
-                <div className="max-w-sm">
-                  <h2 className="mt-12 mb-1 font-medium text-lg">Use cases</h2>
-                  <p className="text-gray-400 mb-4">
-                    Realistic examples to take inspiration from for your own
-                    production projects.
-                  </p>
-                </div>
-                <ul className="grid grid-cols-2 gap-4">
-                  <ListItem
-                    label="Live cursors with chat and reactions"
-                    href="/live-cursors-chat-reactions"
-                    description="Presence"
-                  />
-                  <ListItem
-                    label="Live cursors scrollable page"
-                    href="/live-cursors-scrollable-page"
-                    description="Presence"
-                  />
-                </ul>
-              </div>
-            </div>
-          </>
-        ) : isRunningOnCodeSandbox ? (
-          <>
-            <p className="mt-12 mb-6">
-              To run{" "}
-              <a href="https://liveblocks.io" target="_blank" rel="noreferrer">
-                Liveblocks
-              </a>{" "}
-              examples on CodeSandbox
-            </p>
-            <ul className="list-disc list-inside">
-              <li className="mb-2">
-                Create an account on{" "}
-                <a
-                  href="https://liveblocks.io"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  liveblocks.io
-                </a>
-              </li>
-              <li className="mb-2">
-                Copy your public key from the administration
-              </li>
-              <li className="mb-2">
-                Add a{" "}
-                <a
-                  href="https://codesandbox.io/docs/secrets"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  public key
-                </a>{" "}
-                named{" "}
-                <InlineCodeBlock>
-                  NEXT_PUBLIC_LIVEBLOCKS_PUBLIC_KEY
-                </InlineCodeBlock>{" "}
-                to your CodeSandbox sandbox.
-              </li>
-              <li className="mb-2">
-                Refresh your browser and you should be good to go!
-              </li>
-            </ul>
-          </>
-        ) : (
-          <>
-            <p className="mt-12 mb-6">
-              To run{" "}
-              <a href="https://liveblocks.io" target="_blank" rel="noreferrer">
-                Liveblocks
-              </a>{" "}
-              examples locally
-            </p>
-            <ul className="list-disc list-inside">
-              <li className="mb-2">
-                Install all dependencies with{" "}
-                <SingleLineCodeBlock>npm install</SingleLineCodeBlock>
-              </li>
-              <li className="mb-2">
-                Create an account on{" "}
-                <a
-                  href="https://liveblocks.io"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  liveblocks.io
-                </a>
-              </li>
-              <li className="mb-2">
-                Copy your public key from the administration
-              </li>
-              <li className="mb-2">
-                Create a file named{" "}
-                <InlineCodeBlock>.env.local</InlineCodeBlock> and add your
-                Liveblocks public key as environment variable{" "}
-                <SingleLineCodeBlock>
-                  NEXT_PUBLIC_LIVEBLOCKS_PUBLIC_KEY=pk_test_xx
-                </SingleLineCodeBlock>
-              </li>
-              <li className="mb-2">
-                Run the following command and you should be good to go
-                <SingleLineCodeBlock>npm run dev</SingleLineCodeBlock>
-              </li>
-            </ul>
-          </>
-        )}
-      </main>
-    </div>
+            <li className="flex items-center space-x-2 text-sm bg-gray-100 rounded-md py-2 px-3">
+              <span>Escape</span>
+              <span className="block uppercase font-medium text-xs text-gray-500 rounded border border-gray-300 px-1">
+                esc
+              </span>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <LiveCursorsChatReactions />
+      </RoomProvider>
+      </>
   );
 }
